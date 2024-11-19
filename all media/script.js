8abc34872671cddba901f749127c65ed3e689e19
giresse31 <142321165+giresse31@users.noreply.github.com>
2024-11-20 00:59:46 +0100

    Add files via upload

---
+++ all media/script.js
@@ -0,0 +1,94 @@
+const button = document.getElementById('button');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+const button4 = document.getElementById('button4');
+const button5 = document.getElementById('button5');
+const button6 = document.getElementById('button6');
+const button7 = document.getElementById('button7');
+
+const text = document.getElementById('text');
+const text2 = document.getElementById('text2');
+const text3 = document.getElementById('text3');
+const text4 = document.getElementById('text4');
+const text5 = document.getElementById('text5');
+const text6 = document.getElementById('text6');
+const text7 = document.getElementById('text7');
+
+const point = document.getElementById('point');
+const point2 = document.getElementById('point2');
+const point3 = document.getElementById('point3');
+const point4 = document.getElementById('point4');
+const point5 = document.getElementById('point5');
+const point6 = document.getElementById('point6');
+const point7 = document.getElementById('point7');
+
+
+// redirection 1
+button.addEventListener('click', () => {
+  text.style.visibility = 'hidden';
+  point.style.visibility = 'visible';
+  
+  setTimeout(() => {
+    window.location.href = 'https://t.me/citation_et_conseil';
+  }, 2000);
+});
+
+// redirection 2
+button2.addEventListener('click', () => {
+  text2.style.visibility = 'hidden';
+  point2.style.visibility = 'visible';
+  
+  setTimeout(() => {
+    window.location.href = 'https://motivation-master-class.systeme.io/formation';
+  }, 2000);
+});
+
+// redirection 3
+button3.addEventListener('click', () => {
+  text3.style.visibility = 'hidden';
+  point3.style.visibility = 'visible';
+  
+  setTimeout(() => {
+    window.location.href = 'https://www.facebook.com/share/19hegLT5bf/';
+  }, 2000);
+});
+
+// redirection 4
+button4.addEventListener('click', () => {
+  text4.style.visibility = 'hidden';
+  point4.style.visibility = 'visible';
+  
+  setTimeout(() => {
+    window.location.href = 'https://youtube.com/@citation-conseil-histoire?si=wjXz32DaKHnVaefL';
+  }, 2000);
+});
+
+// redirection 5
+button5.addEventListener('click', () => {
+  text5.style.visibility = 'hidden';
+  point5.style.visibility = 'visible';
+  
+  setTimeout(() => {
+    window.location.href = 'https://www.instagram.com/citation.et.histoire/profilecard/?igsh=MXIyOGpyNXZ3dWpsaw==';
+  }, 2000);
+});
+
+// redirection 6
+button6.addEventListener('click', () => {
+  text6.style.visibility = 'hidden';
+  point6.style.visibility = 'visible';
+  
+  setTimeout(() => {
+    window.location.href = 'https://wa.me/message/RXHDOEYECLZPA1';
+  }, 2000);
+});
+
+// redirection 7
+button7.addEventListener('click', () => {
+  text7.style.visibility = 'hidden';
+  point7.style.visibility = 'visible';
+  
+  setTimeout(() => {
+    window.location.href = 'https://www.tiktok.com/@citations_et_histoire?_t=8rVFQwXhzuQ&_r=1';
+  }, 2000);
+});
